b686c18a2699025102fc69472070b0babf848ee8
C0NC00L <49729272+C0NC00L@users.noreply.github.com>
2021-04-12 11:39:15 -0700

    Fixed some visual stuff, and you cant join chats multiple times now

--- InfoContainer.js
+++ InfoContainer.js
@@ -36,33 +36,39 @@ let chat3 = new Chat("Global Chat 3", "123", "blue",
 router.ChatContainer = [chat1, chat2, chat3]
 
 router.post('/CreateChatFunction',function(req,res) {
-
 	fs.mkdir(path.join(__dirname, 'public/logs/' + req.body.name), (err) => {
 	    if (err) {
 	       //EEXIST is the error code that is given when path is arealdy there
 	       //return console.error(err);
 	    }
 	    else{
-			fs.writeFile('public/logs/' + req.body.name + '/DevChat.txt', "data text", (err) => { 
+			fs.writeFile('public/logs/' + req.body.name + '/DevChat.txt', "Enter a message to start chatting", (err) => { 
 			      
 			    // In case of a error throw err. 
 			    if (err) throw err; 
 			})
-			fs.writeFile('public/logs/' + req.body.name + '/DisplayedChat.txt', "data text", (err) => { 
+			fs.writeFile('public/logs/' + req.body.name + '/DisplayedChat.txt', "Enter a message to start chatting", (err) => { 
 			      
 			    // In case of a error throw err. 
 			    if (err) throw err; 
 			})
 	    }
 	});
+	var inChat = false;
+	router.ChatContainer.forEach(element => {
+      if(element.name == req.body.name){
+        //alert("You're already in that chat");
+        console.log(element.name );
+        inChat = true;
+  		}
+	});
+	if(!inChat){
+		let aNewChat = new Chat(req.body.name, "123", "blue", 
+						(__dirname,'public/logs/' + req.body.name + '/DisplayedChat.txt'), 
+						(__dirname,'public/logs/' + req.body.name + '/DevChat.txt'));
+		router.ChatContainer[router.ChatContainer.length] = aNewChat;
+	}
 
-	let aNewChat = new Chat(req.body.name, "123", "blue", 
-					(__dirname,'public/logs/' + req.body.name + '/DisplayedChat.txt'), 
-					(__dirname,'public/logs/' + req.body.name + '/DevChat.txt'));
-	router.ChatContainer[router.ChatContainer.length] = aNewChat;
-
-	//this.ChatContainer[this.ChatContainer.length] = new Chat()
-    //curChatNum = req.body.number;
     res.json({default:"text"});
 });
 
