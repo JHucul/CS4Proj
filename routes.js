5786b79cd809a315190a99e68ac6ec136ef358ac
Joshua Hucul <hucul.joshua@gmail.com>
2021-04-18 14:05:46 -0700

    no message

--- routes.js
+++ routes.js
@@ -3,13 +3,13 @@ var express = require("express");
 var router = express.Router();
 var formidable = require('formidable');
 var mv = require('mv');
+var path = require('path');
+const http = require('http');
 const fs = require('fs');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); 
 router.use(bodyParser.urlencoded({ extended: true }));  //new Need to add for post
 router.use(bodyParser.json());                          //new Need to add for post
 var info = require("./InfoContainer");
-
-
 
 router.post('/fileupload', function(req, res){
     var form = new formidable.IncomingForm();
@@ -47,27 +47,22 @@ router.post('/setTextLogText', function(req, res){////req.query for get, req.bod
 
 router.post('/clearTextLogText', function(req, res){////req.query for number, req.body for strings
     //info.ChatContainer[curChatNum].textLog = '';
-    fs.writeFile(info.ChatContainer[req.body.num].publicPath,'', (err) => {
-
-      // In case of a error throw err.
-      if (err) throw err;
+    fs.writeFile(info.ChatContainer[req.body.num].publicPath,'', (err) => { 
+        
+      // In case of a error throw err. 
+      if (err) throw err; 
   })
     res.json({default:'text'});
 })
 router.post('/Login', function(req, res){
   fs.readFile('./logins.json', 'utf8', (err, data) => {
     const logins = JSON.parse(data);
-    for(var i=0; i<logins.users.length; i++){
-      logins.users[i].loggedin = false
-    }
+    logins.users.forEach(element => {
+      if(element.username == req.body.name){
+        element.loggedin = false
+      }
+    })
     let present = false
-    // logins.users.forEach(element => {
-    //   if(element.username == req.body.name && element.password == req.body.passwd){
-    //     present = true
-    //     element.loggedin = true
-    //     res.sendStatus(200)
-    //   }
-    // });
     for(var i=0; i<logins.users.length; i++){
       if(logins.users[i].username == req.body.name && logins.users[i].password == req.body.passwd){
         present = true
@@ -75,6 +70,12 @@ router.post('/Login', function(req, res){
         res.sendStatus(200)
       }
     }
+    fs.writeFile('./logins.json', JSON.stringify(logins, null, 4), (err) => {
+      if (err) {
+        console.log(`Error writing file: ${err}`);
+        res.sendStatus(500)
+      }
+    })
     if(!present)
       res.sendStatus(403)
   });
@@ -89,9 +90,11 @@ router.post('/Register', function(req, res){
 
         // parse JSON string to JSON object
         const logins = JSON.parse(data);
-        // for(var i=0; i<logins.users.length; i++){
-        //   logins.users[i].loggedin = false
-        // }
+        logins.users.forEach(element => {
+          if(element.username == req.body.name){
+            element.loggedin = false
+          }
+        })
         // add a new record
         let present = false
         logins.users.forEach(element => {
@@ -104,7 +107,7 @@ router.post('/Register', function(req, res){
           logins.users.push({
             username: req.body.name,
             password: req.body.passwd,
-            // loggedin: true
+            loggedin: true
           });
           // write new data back to the file
           fs.writeFile('./logins.json', JSON.stringify(logins, null, 4), (err) => {
@@ -118,5 +121,29 @@ router.post('/Register', function(req, res){
     }
   });
 })
-
+router.get('/Chat', function(req, res){
+  fs.readFile('./logins.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+      res.sendStatus(500)
+    } else {
+      const logins = JSON.parse(data);
+      let present = false
+      logins.users.forEach(element => {
+        if(element.username == req.query.name && element.loggedin == true){
+          present = true
+        }
+      })
+      if(!present){
+        console.log('fail')
+        res.sendStatus(403)
+      }
+      else{
+        console.log('succ')
+        res.sendFile('public/views/Chat.html', {root: __dirname })
+      }
+    }
+  })
+})
 module.exports = router;
+
